25aec93d510bbfff50e286cd30dd97fb4db3731b
tom <tom2wheeler@gmail.com>
2017-07-23 18:38:38 -0700

    Added embedded LDAP authentication

--- src/main/java/org/suliga/badlands/config/MainSecurityConfig.java
+++ src/main/java/org/suliga/badlands/config/MainSecurityConfig.java
@@ -1,14 +1,21 @@
 package org.suliga.badlands.config;
 
+import java.util.Arrays;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
 
 @Configuration
 @EnableWebSecurity
@@ -51,16 +58,38 @@ public class MainSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
- /*       auth
-            .inMemoryAuthentication()
-                .withUser("user").password("user").roles("USER").and()
-                .withUser("tom").password("tom").roles("USER", "ADMIN", "DBA").and()
-    	  		.withUser("admin").password("Secret1").roles("ADMIN").and()
-    			.withUser("dba").password("Secret1").roles("DBA"); */
     	auth.jdbcAuthentication()
     		.dataSource(dataSource)
     		.passwordEncoder(new BCryptPasswordEncoder());
     }
+    
+    /*
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().fullyAuthenticated()
+				.and()
+			.formLogin();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource(contextSource())
+				.passwordCompare()
+					.passwordEncoder(new LdapShaPasswordEncoder())
+					.passwordAttribute("userPassword");
+	}
+
+	@Bean
+	public DefaultSpringSecurityContextSource contextSource() {
+		return  new DefaultSpringSecurityContextSource(Arrays.asList("ldap://localhost:8389/"), "dc=springframework,dc=org");
+	}
+	*/	
 }
 
 
