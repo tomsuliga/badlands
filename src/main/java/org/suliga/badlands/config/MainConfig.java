91310e4cf49ddb050e74397817eefa67011079ed
tom <tom2wheeler@gmail.com>
2017-07-24 06:01:27 -0700

    Added i18n example

--- src/main/java/org/suliga/badlands/config/MainConfig.java
+++ src/main/java/org/suliga/badlands/config/MainConfig.java
@@ -1,8 +1,34 @@
 package org.suliga.badlands.config;
 
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class MainConfig {
-
+public class MainConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US);
+		return slr;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang"); // http://localhost:8080/i18n?lang=de
+		return lci;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 }
+
